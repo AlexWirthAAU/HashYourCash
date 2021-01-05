8ecd100a87a5bc307422dc50fb622c2f1dcf6a88
Jana Siebert <jsiebert@edu.aau.at>
2021-01-06 00:25:20 +0100

    add payment overview table

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatInputModule,
-    MatRadioModule
+    MatRadioModule,
+    MatTableModule
   ]
 })
 export class MaterialModule {}
--- src/app/payments/overview/payments.overview.component.ts
+++ src/app/payments/overview/payments.overview.component.ts
@@ -1,17 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {faPlus} from '@fortawesome/free-solid-svg-icons';
+import {ApiService} from '../../services/api.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {Payment} from '../../model/payment';
 
 @Component({
   selector: 'app-payments-overview',
   templateUrl: './payments.overview.component.html',
 })
-export class PaymentsOverviewComponent{
+export class PaymentsOverviewComponent implements OnInit {
   faPlus = faPlus;
 
-  constructor(private router: Router){}
+  dataSource = new MatTableDataSource<Payment>();
+  displayedColumns: string[];
 
-  public fabAddClick(){
+  constructor(private api: ApiService, private router: Router) {
+    this.displayedColumns = [
+      'type',
+      'amount',
+    ];
+  }
+
+  public fabAddClick() {
     this.router.navigateByUrl('payments/add');
+  }
+
+  ngOnInit(): void {
+    this.api.getPayments(1, 1).subscribe(payments => {
+      this.dataSource.data = payments;
+    });
   }
 }
