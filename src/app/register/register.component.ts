585f75f29b57a64548e8ae48638c7cc57df76de7
AlexWirthAAU <alexwi@edu.aau.at>
2021-01-12 11:02:49 +0100

    email change

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -16,6 +16,7 @@ export class RegisterComponent implements OnInit {
   passwordErr: string = null;
   emailErr: string = null;
 
+
   constructor(public api: ApiService, public router: Router) {
     this.formData = new FormGroup({
       first_name: new FormControl('', Validators.required),
@@ -24,12 +25,10 @@ export class RegisterComponent implements OnInit {
       password: new FormControl('', Validators.required),
       confirm_password: new FormControl('', Validators.required)
     })
-
-    this.getAllEmails();
   }
 
   ngOnInit(): void {
-
+    this.getAllEmails();
   }
 
   getAllEmails() {
@@ -53,6 +52,7 @@ export class RegisterComponent implements OnInit {
         if (this.checkEmail()) {
           this.api.register(this.formData.value).subscribe(
             response => {
+              console.log("Successfully registered")
               this.router.navigate(['/'])
             }, //success path
             error => {
@@ -66,7 +66,7 @@ export class RegisterComponent implements OnInit {
     }
   }
 
-  checkEmail(): boolean {
+  checkEmail() {
     let resultArr = this.userEmails.filter(x => x && x.email && x.email === this.formData.value.email);
     if (resultArr.length > 0) {
       this.emailErr = "E-Mail Adresse ist schon vergeben."
@@ -75,6 +75,10 @@ export class RegisterComponent implements OnInit {
       this.emailErr = null;
       return true;
     }
+
   }
+
+
+
 
 }
