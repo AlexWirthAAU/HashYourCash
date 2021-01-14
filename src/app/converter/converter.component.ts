60a1d9add1a05a94992905ddfb78ac912b08e4da
bego99 <begoja@edu.aau.at>
2021-01-14 18:13:31 +0100

    Verschönerungen

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -11,10 +11,10 @@ export class ConverterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  
+
     const select = document.querySelectorAll("select");
     const input: any = document.querySelectorAll("input");
-   
+
     const api_URL = "http://data.fixer.io/api/latest?access_key=87492957e71943fca9a7a8dd3e18e935";
     //const api_URL = "https://api.nomics.com/v1"
     let html = "";
@@ -31,7 +31,7 @@ export class ConverterComponent implements OnInit {
       const rates = data.rates;
       //console.log(rates);
 
-    
+
       arrKeys.map(item => {
         return html += `<option value=${item}>${item}</option>`;
       });
@@ -39,30 +39,30 @@ export class ConverterComponent implements OnInit {
 
       for (let i = 0; i < select.length; i++) {
         select[i].innerHTML = html;
-   
-      //console.log(rates[select[0].value]) //rates
 
-    
-      input[0].addEventListener("keyup", () => {
-        input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
-      })
-
-      input[1].addEventListener("keyup", () => {
-        input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];;
-      })
+        //console.log(rates[select[0].value]) //rates
 
 
-      select[0].addEventListener("change", () => {
-        input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
-      })
+        input[0].addEventListener("keyup", () => {
+          input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
+        })
 
-      select[1].addEventListener("change", () => {
-        input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];;
-      })
+        input[1].addEventListener("keyup", () => {
+          input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];;
+        })
 
-    };
 
-  }
+        select[0].addEventListener("change", () => {
+          input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
+        })
+
+        select[1].addEventListener("change", () => {
+          input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];;
+        })
+
+      };
+
+    }
     currency();
   }
 }
