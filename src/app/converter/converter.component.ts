c13adf261c6d8dbdc3f862369d392ae16c074266
bego99 <begoja@edu.aau.at>
2021-01-21 16:22:37 +0100

    Changed API to https

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -15,52 +15,53 @@ export class ConverterComponent implements OnInit {
     const select = document.querySelectorAll("select");
     const input: any = document.querySelectorAll("input");
 
-    const api_URL = "http://data.fixer.io/api/latest?access_key=87492957e71943fca9a7a8dd3e18e935";
-    //const api_URL = "https://api.nomics.com/v1"
+    //const api_URL = "http://data.fixer.io/api/latest?access_key=87492957e71943fca9a7a8dd3e18e935";
+    //const api_URL = "https://free.currconv.com/api/v7/currencies?apiKey=574adf64e94097befbe7"
     let html = "";
+    const api_URL = "https://api.exchangeratesapi.io/latest?access_key=87492957e71943fca9a7a8dd3e18e935&base=ISK"
 
 
 
-    async function currency() {
+      async function currency() {
       const result = await fetch(api_URL);
-      //console.log(result); 
+      console.log(result); 
       const data = await result.json();
-      //console.log(data.rates);
+      console.log(data);
+      console.log(data.rates);
       const arrKeys = Object.keys(data.rates)
-      //console.log(arrKeys[2]);
+      console.log(arrKeys[2]);
       const rates = data.rates;
-      //console.log(rates);
+      console.log(rates);
 
 
-      arrKeys.map(item => {
-        return html += `<option value=${item}>${item}</option>`;
-      });
+        arrKeys.map(item => {
+          return html += `<option value=${item}>${item}</option>`;
+        });
       // gibt alle currencies auf html
 
-      for (let i = 0; i < select.length; i++) {
-        select[i].innerHTML = html;
+       for (let i = 0; i < select.length; i++) {
+         select[i].innerHTML = html;
 
-        //console.log(rates[select[0].value]) //rates
+         console.log(rates[select[0].value]) //rates
 
 
-        input[0].addEventListener("keyup", () => {
-          input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
+         input[0].addEventListener("keyup", () => {
+           input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
+         })
+
+         input[1].addEventListener("keyup", () => {
+           input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];;
+         })
+
+         select[0].addEventListener("change", () => {
+         input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
         })
 
-        input[1].addEventListener("keyup", () => {
-          input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];;
-        })
+         select[1].addEventListener("change", () => {
+           input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];;
+         })
 
-
-        select[0].addEventListener("change", () => {
-          input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
-        })
-
-        select[1].addEventListener("change", () => {
-          input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];;
-        })
-
-      };
+       };
 
     }
     currency();
